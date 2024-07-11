fb55b4d37f4cf52bb2941504decf3dfe2e9435a0
Will Stewart <fevenkitsune@gmail.com>
2024-07-11 17:20:18 -0400

    New player movement, inputs, and camera controls
    Implemented improved movement system, added test key for use animations, moved zoom script from previous player controller.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
-
-# Define the size of each grid cell
-const GRID_SIZE = 8
+@export var speed = 16
 
 # Movement directions
 enum Direction {
@@ -14,75 +12,79 @@ enum Direction {
 }
 
 var facing: Direction
-
-# Current target position
-var target_position: Vector2
-
-# Flag to check if the character is moving
-var is_moving = false
-
-func _ready():
-	# Initialize the target position to the current position
-	target_position = position
+var moving: bool
+var interacting: bool
+var queue = [null] # https://www.reddit.com/r/godot/comments/1anpsdz/a_proper_4way_movement_system_for_keyboard_input/
 
 func _physics_process(delta):
-	# Handle input only if not moving
-	if not is_moving:
-		if Input.is_action_pressed("ui_up"):
-			move_to(Direction.UP)
-		elif Input.is_action_pressed("ui_down"):
-			move_to(Direction.DOWN)
-		elif Input.is_action_pressed("ui_left"):
-			move_to(Direction.LEFT)
-		elif Input.is_action_pressed("ui_right"):
-			move_to(Direction.RIGHT)
-		else:
-			play_anim_idle()
+	for input in ["ui_left", "ui_right", "ui_up", "ui_down", "ui_select"]:
+		if Input.is_action_just_pressed(input): queue.push_back(input)
+		if Input.is_action_just_released(input): queue.erase(input)
 
-	# Smoothly move towards the target position
-	if is_moving:
-		velocity = (target_position - position).normalized() * GRID_SIZE * 2
-		move_and_slide()
-		if position.distance_to(target_position) < 1:
-			position = target_position
-			is_moving = false
+	var direction = Vector2.ZERO
+	match queue.back():
+		"ui_left":
+			direction = Vector2.LEFT
+			facing = Direction.LEFT
+			moving = true
+		"ui_right":
+			direction = Vector2.RIGHT
+			facing = Direction.RIGHT
+			moving = true
+		"ui_up":
+			direction = Vector2.UP
+			facing = Direction.UP
+			moving = true
+		"ui_down":
+			direction = Vector2.DOWN
+			facing = Direction.DOWN
+			moving = true
+		"ui_select":
+			moving = false
+			interacting = true
+		_:
+			direction = Vector2.ZERO
+			moving = false
+			interacting = false
 
-func play_anim_idle():
+	velocity = direction * speed
+
+	move_and_slide()
+
+var zoom = 0
+
+func _process(delta):
+	if Input.is_action_just_pressed("scroll_up"):
+		zoom = clamp(zoom+1,0,2)
+	elif Input.is_action_just_pressed("scroll_down"):
+		zoom = clamp(zoom-1,0,2)
+		
+	match zoom:
+		0:
+			$Camera2D.zoom = Vector2(1,1)
+		1:
+			$Camera2D.zoom = Vector2(2,2)
+		2:
+			$Camera2D.zoom = Vector2(3,3)
+	
+	if moving:
+		play_anim("move")
+	elif interacting:
+		play_anim("interact")
+	else:
+		play_anim("idle")
+
+func play_anim(action):
 	match facing:
 		Direction.UP:
 			_animated_sprite.flip_h = false
-			_animated_sprite.play("idle_back")
+			_animated_sprite.play(action+"_back")
 		Direction.DOWN:
 			_animated_sprite.flip_h = false
-			_animated_sprite.play("idle_front")
+			_animated_sprite.play(action+"_front")
 		Direction.LEFT:
 			_animated_sprite.flip_h = true
-			_animated_sprite.play("idle_side")
+			_animated_sprite.play(action+"_side")
 		Direction.RIGHT:
 			_animated_sprite.flip_h = false
-			_animated_sprite.play("idle_side")
-
-func move_to(direction: int):
-	match direction:
-		Direction.UP:
-			_animated_sprite.flip_h = false
-			_animated_sprite.play("move_back")
-			target_position = position + Vector2(0, -GRID_SIZE)
-		Direction.DOWN:
-			_animated_sprite.flip_h = false
-			_animated_sprite.play("move_front")
-			target_position = position + Vector2(0, GRID_SIZE)
-		Direction.LEFT:
-			_animated_sprite.flip_h = true
-			_animated_sprite.play("move_side")
-			target_position = position + Vector2(-GRID_SIZE, 0)
-		Direction.RIGHT:
-			_animated_sprite.flip_h = false
-			_animated_sprite.play("move_side")
-			target_position = position + Vector2(GRID_SIZE, 0)
-	
-	facing = direction
-	
-	# Check for collision and set is_moving to true if no collision
-	if not is_moving:
-		is_moving = true
+			_animated_sprite.play(action+"_side")
