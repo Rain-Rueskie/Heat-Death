25be6e482c5c548f374442f0c0bac6f68fbb67d4
Will Stewart <fevenkitsune@gmail.com>
2025-01-15 10:53:16 -0500

    added initial grid mining, but major issues, needs refactoring

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
+@onready var _tick_digging = $TickDigging
 @export var speed = 16
+@export var mining_speed = 15
 @export var camera = Camera2D
+@onready var world: MineableLayer = $"../Level/FG"
 
 # Movement directions
 enum Direction {
@@ -12,10 +15,14 @@ enum Direction {
 	RIGHT
 }
 
-var facing: Direction
+var facing: Vector2
 var moving: bool
 var interacting: bool
 var queue = [null] # https://www.reddit.com/r/godot/comments/1anpsdz/a_proper_4way_movement_system_for_keyboard_input/
+
+func _ready():
+	var mask: int = world.tile_set.get_physics_layer_collision_layer(0)
+	print(mask)
 
 func _physics_process(delta):
 	for input in ["ui_left", "ui_right", "ui_up", "ui_down", "ui_select"]:
@@ -26,32 +33,37 @@ func _physics_process(delta):
 	match queue.back():
 		"ui_left":
 			direction = Vector2.LEFT
-			facing = Direction.LEFT
+			facing = Vector2.LEFT
 			moving = true
 		"ui_right":
 			direction = Vector2.RIGHT
-			facing = Direction.RIGHT
+			facing = Vector2.RIGHT
 			moving = true
 		"ui_up":
 			direction = Vector2.UP
-			facing = Direction.UP
+			facing = Vector2.UP
 			moving = true
 		"ui_down":
 			direction = Vector2.DOWN
-			facing = Direction.DOWN
+			facing = Vector2.DOWN
 			moving = true
 		"ui_select":
 			moving = false
 			interacting = true
+			if _tick_digging.is_stopped():
+				_tick_digging.start()
 		_:
 			direction = Vector2.ZERO
 			moving = false
 			interacting = false
+			_tick_digging.stop()
 
 	velocity = direction * speed
 
-	# TODO: Lock player to grid
+	# TODO: Lock player to grid?
 	move_and_slide()
+	
+	
 
 
 func _process(delta):
@@ -64,15 +76,44 @@ func _process(delta):
 
 func play_anim(action):
 	match facing:
-		Direction.UP:
+		Vector2.UP:
 			_animated_sprite.flip_h = false
 			_animated_sprite.play(action+"_back")
-		Direction.DOWN:
+		Vector2.DOWN:
 			_animated_sprite.flip_h = false
 			_animated_sprite.play(action+"_front")
-		Direction.LEFT:
+		Vector2.LEFT:
 			_animated_sprite.flip_h = true
 			_animated_sprite.play(action+"_side")
-		Direction.RIGHT:
+		Vector2.RIGHT:
 			_animated_sprite.flip_h = false
 			_animated_sprite.play(action+"_side")
+
+
+func _on_tick_digging_timeout() -> void:
+	# https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(position, position + facing * 8, 1)
+	query.exclude = [self]
+	query.collide_with_areas = false
+	query.collide_with_bodies = true
+	var result = space_state.intersect_ray(query)
+	if result == {}:
+		return
+	print(result)
+	
+	var target_cell = world.local_to_map(result["position"] + (facing * 0.1)) # penetrate ray slightly to ensure correct tile is detected.
+	print(target_cell)
+	var toughness = world.get_grid_value(target_cell)
+	if not toughness:
+		print("cell data not found")
+		return
+	# Right now the class has hard coded to just return toughness, so we don't need to check what data the cell has.
+	# In the future if we want more data, we will want to check what values that data has and if it has a toughness value.
+	
+	print(str(toughness), "->", str(toughness - 15))
+	world.set_grid_value(target_cell, toughness - 15)
+	
+	if toughness - 15 <= 0:
+		print("Broke cell")
+		world.set_cell(target_cell)
---
+++ scenes/the_lab/fg.gd
@@ -0,0 +1,31 @@
+extends TileMapLayer
+class_name MineableLayer
+
+var toughness = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var dimensions = get_used_rect().size
+	print(dimensions)
+	for y in dimensions.y:
+		toughness.append([])
+		for x in dimensions.x:
+			var cell = Vector2i(x, y)
+			var data = get_cell_tile_data(cell)
+			
+			if not data: continue
+			
+			var cell_toughness = data.get_custom_data("toughness")
+			if not cell_toughness: continue
+			print("Assigned toughness value of ", str(cell_toughness), " to ", str(cell))
+			toughness[y].append(cell_toughness)
+
+func set_grid_value(position: Vector2i, object: int):
+	toughness[position.y][position.x] = object
+	
+func get_grid_value(position: Vector2i) -> int:
+	return toughness[position.y][position.x]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
